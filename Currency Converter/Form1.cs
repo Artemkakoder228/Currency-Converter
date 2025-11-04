77953f5d9681a1c23bd2ee30c26fbedcff0725bc
Artemkakoder228 <24b_oas@liseum.ztu.edu.ua>
2025-11-04 10:56:41 +0200

    Add project files.

---
+++ Currency Converter/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = "Курс актуальний на " + DateTime.Now.ToString("dd.MM.yyyy");
+        }
+
+        private void lblDate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
